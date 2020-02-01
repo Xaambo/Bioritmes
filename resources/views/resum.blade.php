b0c031b97ab8de22d77ba6b39733967342bae2cb
Jordi Joan <jordi.joan@mataro.epiaedu.cat>
2020-02-01 22:46:55 +0100

    prova

--- resources/views/resum.blade.php
+++ resources/views/resum.blade.php
@@ -10,6 +10,7 @@
                 <h1 class="display-4">Resultats</h1>
                 <p>Portes viu un total de: {{$dies}}</p>
                 <p>Això significa que els teus bioritmes estan en el següent estat:</p>
+                
             </div>
         </div>
     </div>
