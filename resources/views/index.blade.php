3427290aee60f1272e67194f9e3845398b504f62
Jordi Joan <jordi.joan@mataro.epiaedu.cat>
2020-02-01 22:59:22 +0100

    hol

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,6 @@
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
                 <h1 class="display-4">Biorritme</h1>
-                
                 <p>Benvingut: {{$nom}}</p>
             </div>
         </div>
